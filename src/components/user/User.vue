050d7f9bae3ef44b17302ff6e3e3cfe7c829faa2
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-09 17:15:55 -0500

    added Header.vue

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,18 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+        <h1>The User Page</h1>
+        <button class="btn btn-primary" @click="navigateToHome">Go to /home</button>
+    </div>
+    
 
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome: function(){
+            this.$router.push({path:'/'});
+        }
+    }
+}
+</script>
